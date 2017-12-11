3c3085c12cc9001b00e1d32d9b542f00c2e9181b
arslansadiq <m.arslansadiq@yahoo.com>
2017-12-11 22:52:41 +0100

    Update Code.m

--- Assignment no. 3/Code.m
+++ Assignment no. 3/Code.m
@@ -6,6 +6,7 @@ time = 1 : 0.001 : time_end;
 initial_condition = 20;
 time_step = [0.5, 0.25, 0.125, 0.0625, 0.03125];
 analytical_solution = 200./(20 - 10*exp(-7*time));
+stability_table = [time_step;ones(6,5)]; 
 
 %%%%%%%%%%%%       Implementation of Explicit Euler Method's from Worksheet 2      %%%%%%%%%%%%%
 
@@ -18,28 +19,34 @@ for i=1:size(time_step , 2)
     plot(0:time_step(i):time_end , euler_exp_approx , 'DisplayName' , strcat('Time Step: ' , string(i)));
     exact_solution_EE = 200./(20-10*exp(-7*(0 : time_step(i) : time_end)));
     exact_error_EE(i) = Error(euler_exp_approx , time_step(i) , time_end , exact_solution_EE);
+    if exact_error_EE(i)>2
+        stability_table(2,i) = 0;
+    end
     hold on;
 end
 plot(time , analytical_solution , 'DisplayName' , 'Analytical Solution');
 legend show
 Explicit_Euler_Table = array2table(Error_Table(exact_error_EE) , 'RowNames' , {'dt','error','error red'})
 
-%%%%%%%%%%%%       Implementation of Explicit Heun Method's from Worksheet 2      %%%%%%%%%%%%%
+%%%%%%%%%%%%       Implementation of Heun Method's from Worksheet 2      %%%%%%%%%%%%%
 
 exact_error_EH = zeros(1 , size(time_step , 2));
 for i=1:size(time_step , 2)
-    heun_exp_approx = explicit_heun(time_step(i) , time_end , initial_condition);
+    heun_exp_approx = heun(time_step(i) , time_end , initial_condition);
     figure(2)
     axis([0 5 0 20]);
-    title('Numerical Approximation Method: Explicit Heun Method');
+    title('Numerical Approximation Method: Heun Method');
     plot(0:time_step(i):time_end , heun_exp_approx , 'DisplayName' , strcat('Time Step: ' , string(i)));
     exact_solution_EH = 200./(20-10*exp(-7*(0 : time_step(i) : time_end)));
     exact_error_EH(i) = Error(heun_exp_approx , time_step(i) , time_end , exact_solution_EH);
+    if exact_error_EH(i)>2
+        stability_table(3,i) = 0;
+    end
     hold on;
 end
 plot(time , analytical_solution , 'DisplayName' , 'Analytical Solution');
 legend show
-Explicit_Heun_Table = array2table(Error_Table(exact_error_EH) , 'RowNames' , {'dt','error','error red'})
+Heun_Table = array2table(Error_Table(exact_error_EH) , 'RowNames' , {'dt','error','error red'})
 
 %%%%%%%%%%%%      Implementation for Implicit Euler Method's        %%%%%%%%%%%%
 
@@ -52,6 +59,9 @@ for i=1:size(time_step , 2)
     plot(0:time_step(i):time_end , euler_imp_approx , 'DisplayName' , strcat('Time Step: ' , string(i)));
     exact_solution_IE = 200./(20-10*exp(-7*(0 : time_step(i) : time_end)));
     exact_error_IE(i) = Error(euler_imp_approx , time_step(i) , time_end , exact_solution_IE);
+    if exact_error_IE(i)>2
+        stability_table(4,i) = 0;
+    end
     hold on;
 end
 plot(time , analytical_solution , 'DisplayName' , 'Analytical Solution');
@@ -64,11 +74,14 @@ exact_error_adam_moltn = zeros(1 , size(time_step , 2));
 for i=1:size(time_step , 2)
     adam_moulton_approx = adam_moulton(time_step(i) , time_end , initial_condition);
     figure(4)
-    axis([0 5 -40 40]);
+    axis([0 5 0 20]);
     title('Numerical Approximation Method: 2nd Order Adam Moulton Method');
     plot(0:time_step(i):time_end , adam_moulton_approx , 'DisplayName' , strcat('Time Step: ' , string(i)));
     exact_solution_AM = 200./(20-10*exp(-7*(0 : time_step(i) : time_end)));
     exact_error_adam_moltn(i) = Error(adam_moulton_approx , time_step(i) , time_end , exact_solution_AM);
+    if exact_error_adam_moltn(i)>2 || isnan(exact_error_adam_moltn(i))
+        stability_table(5,i) = 0;
+    end
     hold on;
 end
 plot(time , analytical_solution , 'DisplayName' , 'Analytical Solution');
@@ -86,6 +99,9 @@ for i=1:size(time_step , 2)
     plot(0:time_step(i):time_end , L1_adam_moulton_approx , 'DisplayName' , strcat('Time Step: ' , string(i)));
     exact_solution_AM_L1 = 200./(20-10*exp(-7*(0 : time_step(i) : time_end)));
     exact_error_adam_moltn_L1(i) = Error(L1_adam_moulton_approx , time_step(i) , time_end , exact_solution_AM_L1);
+    if exact_error_adam_moltn_L1(i)>2
+        stability_table(6,i) = 0;
+    end
     hold on;
 end
 plot(time , analytical_solution , 'DisplayName' , 'Analytical Solution');
@@ -104,11 +120,19 @@ for i=1:size(time_step , 2)
     hold on;
     exact_solution_AM_L2 = 200./(20-10*exp(-7*(0 : time_step(i) : time_end)));
     exact_error_adam_moltn_L2(i) = Error(L2_adam_moulton_approx , time_step(i) , time_end , exact_solution_AM_L2);
+    if exact_error_adam_moltn_L2(i)>2
+        stability_table(7,i) = 0;
+    end
 end
 plot(time , analytical_solution , 'DisplayName' , 'Analytical Solution');
 legend show
+legend('Location' , 'southwest')
 Adam_Moulton_L2_Table = array2table(Error_Table(exact_error_adam_moltn_L2) , 'RowNames' , {'dt','error','error red'})
-
+Stability_Table = array2table(stability_table , 'RowNames' , {'Time Step','Explicit Euler','Heun','Implicit Euler',...
+    'Adams Moulton','Adams Moulton L1','Adams Moulton L12'})
+disp('Following are the pointers for Stability table');
+disp('0 -> Unstability');
+disp('1 -> Stability');
 %%%%%%%%%%%          Explicit Euler Method's Definition        %%%%%%%%%%%%%
 
 function approx_value = explicit_euler(dt , time_end , y0)
@@ -119,9 +143,9 @@ for i = 1:(size(approx_value , 2)-1)
 end
 end
 
-%%%%%%%%%%%          Explicit Heun Method's Definition        %%%%%%%%%%%%%
+%%%%%%%%%%%          Heun Method's Definition        %%%%%%%%%%%%%
 
-function approx_value = explicit_heun(dt , time_end , y0)
+function approx_value = heun(dt , time_end , y0)
 approx_value = zeros(1 , (time_end./dt)+1);
 approx_value(1) = y0;
 for i = 1:(size(approx_value , 2)-1)
@@ -144,7 +168,7 @@ end
 
 %%%%%%%%%%%          2nd Order Adam Moulton Method's Definition        %%%%%%%%%%%%%
 
-function approx_value = adam_moulton(dt , time_end , y0)
+function approx_value = adam_moulton(dt , time_end , y0, a)
 approx_value =  zeros(1 , (time_end./dt)+1);
 approx_value(1) = y0;
 for i = 1:(size(approx_value , 2)-1)
@@ -178,14 +202,18 @@ end
 
 function yi = newton_raphson_method(accuracy_limit , yi_1 , h , g , gp)
 error = 5;
-initial_guess = yi_1+25;
+initial_guess = yi_1+25*rand;
 itirations=0;
-while (error >= accuracy_limit)&& itirations < 100
+while (error >= accuracy_limit) && itirations < 300
     initial_guess = initial_guess - g(initial_guess , yi_1 , h)/gp(initial_guess , h);
     error = abs(g(initial_guess , yi_1 , h));
     itirations=itirations+1;
 end
-yi = initial_guess;
+if error>accuracy_limit
+    yi = NaN;
+else
+    yi = initial_guess;
+end
 end
 
 %%%%%%%%%%%      Function for G(x) for Implicit Euler's Method      %%%%%%%%%%%
